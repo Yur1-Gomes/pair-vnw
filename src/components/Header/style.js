dbaf986ad28e3f714a3c49ae11dc54d81a366012
Yur1-Gomes <yuri2912@hotmail.com>
2022-06-21 20:16:25 -0300

    started header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,6 +1,44 @@
 import styled from 'styled-components'
-// import { Theme } from '../../assets'
+import { Theme } from '../../assets'
 
-export const Content = styled.div``
-
-export const Title = styled.h1``
+export const Content = styled.div`
+   display: flex;
+   flex-direction: column;
+   width: 100%;
+   height: 400px;
+   background-image: url(${Theme.images.kitchenP});
+   background-repeat: no-repeat;
+   background-size: cover;
+   height: 85.88rem;
+   margin: 2.25rem;
+`
+export const Logo = styled.img`
+   height: 3.8rem;
+`
+export const Nav = styled.nav`
+   display: flex;
+   margin-top: 4.4rem;
+   margin-left: 3.94rem;
+`
+export const Ul = styled.ul`
+   display: flex;
+   align-items: center;
+   margin-left: 57rem;
+`
+export const Li = styled.li`
+   margin-left: 5.5rem;
+   font-size: 1.5rem;
+   list-style: none;
+`
+export const Button = styled.button`
+   height: 5.05rem;
+   width: 16.4rem;
+   margin-left: 5.5rem;
+   border: solid black 5px;
+   background-color: #d8d2d6;
+   font-size: 1.5rem;
+`
+export const Title = styled.h2`
+   font-size: 9.37rem;
+   margin: 35rem 0 0 38rem;
+`
