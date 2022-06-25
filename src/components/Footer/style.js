e985f7b528e6ae058cc23e2839ec73e83bf4c3b3
Yur1-Gomes <yuri2912@hotmail.com>
2022-06-25 01:01:35 -0300

    started footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,6 +1,40 @@
 import styled from 'styled-components'
 // import { Theme } from '../../assets'
 
-export const Content = styled.div``
-
-export const Title = styled.h2``
+export const Content = styled.div`
+  height: 25rem;
+  width: 100%;
+`
+export const BottomBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 8.75rem;
+`;
+export const Social = styled.div`
+  width: 17.25rem;
+  margin-left: 6.12rem;
+  display: flex;
+  justify-content: space-around;
+`;
+export const Media = styled.img`
+  height: 3.5rem;
+`;
+export const BottomList = styled.ul`
+  display: flex;
+  align-items: center;
+  margin-right: 8.75rem;
+  list-style: none;
+`;
+export const Li = styled.li`
+  margin-left: 5.5rem;
+  font-size: 1.5rem;
+`;
+export const Disclaimer = styled.div`
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8.5rem;
+  background-color: #446061;
+`; 
